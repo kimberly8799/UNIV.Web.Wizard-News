6798d3ee284052a790e82197f0cc9011ef589d26
Neven Nguyen <nevenng@gmail.com>
2023-05-03 21:06:24 -0700

    defining postbanks functions

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+
+
+app.use(morgan('dev'));
+
 
 app.get("/", (req, res) => res.send("Hello World!"));
+
+
 
 const PORT = 1337;
 
 app.listen(PORT, () => {
   console.log(`App listening in port ${PORT}`);
 });
-
 
