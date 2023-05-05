347fa969c2a900e2ec992f1a23e4e6cda7700c03
Neven Nguyen <nevenng@gmail.com>
2023-05-04 20:22:24 -0700

    linked posts

--- app.js
+++ app.js
@@ -1,12 +1,36 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const postBank = require("./postBank");
 
 
 app.use(morgan('dev'));
 
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req,res) => {
+
+const posts = postBank.list();
+
+const html = `<!DOCTYPE HTML>
+<html>
+<head>
+<title>Wizard News </title>
+</head>
+
+<body>
+<ul>
+  ${posts.map(post => `<li>${post.title, post.content}</li>`)}
+</ul>
+
+
+</body>
+</html>`;
+
+res.send(html);
+
+}); 
+
+
 
 
 
